67062a04ac5228afd005b9b9a311612338af5b60
BethMwangi <wanjikumwangib@gmail.com>
2016-06-17 10:34:35 +0300

    embedded hello video

--- flaskvid/app.py
+++ flaskvid/app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app route('/')
+@app.route('/')
 def homepage():
     return """
     <h1> An app that views Youtube videos</h1>
-    <i frame src= "https://www.youtube.com/watch?v=YQHsXMglC9A width=853" height="480" frameborder="0" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/YQHsXMglC9A" width="853" height="480" frameborder="0" allowfullscreen></iframe>
     """
 
 if __name__ == '__main__':
